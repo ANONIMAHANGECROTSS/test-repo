2963c3ed36191a8c8780a90a9319a241b1a428c6
ANONIMAHANGECROTSS <heorld03@gmail.com>
2026-05-10 12:29:50 +0700

    Upload Footer.tsx via RepoFlow

---
+++ portsequence/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="mt-auto py-8 text-center bg-white">
+      <p className="text-sm text-gray-500">© 2026 Kevin Velvet.</p>
+    </footer>
+  );
+}
